5712a421e1a152b8e10d4d0854de225f34b0266e
ozge <yavasozge@gmail.com>
2017-08-11 20:21:23 +0300

    new elements are added

--- my_javascript_sack.js
+++ my_javascript_sack.js
@@ -25,19 +25,10 @@ UTILS = {
   randomIntFromInterval: function (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   },
-  setWindowBlur: function () {
-    window.onblur = function () {
-      window.blurred = true;
-    };
-    window.onfocus = function () {
-      window.blurred = false;
-    };
-  },
   factorial: function (n) {
     if (n === 0) {
       return 1;
     }
-
     // This is it! Recursion!!
     return n * UTILS.factorial(n - 1);
   },
@@ -47,13 +38,56 @@ UTILS = {
     for (i = 0; i < row; i++) {
       myarray[i] = new Array(column);
     }
-  }, 
-  encodeData(data) {
+  },
+  encodeData: function (data) {
     var params = Object.keys(data).map(function (key) {
       return [key, data[key]].map(encodeURIComponent).join("=");
     }).join("&");
     return params + '&key=' + APIKey;
+  },
+  postData: function (post, url, success, error) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var res = JSON.parse(xmlhttp.responseText);
+        if (res.OK) {
+          success(res);
+        } else {
+          error(res);
+        }
+      } else {
+        if (!!xmlhttp.responseText) {
+          var res = JSON.parse(xmlhttp.responseText);
+          console.log(res);
+        }
+      }
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(post));
+  },
+  setWindowBlur: function () {
+    window.onblur = function () {
+      window.blurred = true;
+    };
+    window.onfocus = function () {
+      window.blurred = false;
+    };
+  },
+  imgModal: function () {
+    var btnlist = document.getElementsByClassName('img-modal-btn');
+    var panel = document.getElementsByClassName('modal-panel')[0];
+    for (var i = 0; i < btnlist.length; i++) {
+      var btn = btnlist[i];
+      btn.addEventListener('click', function () {
+        var img = new Image();
+        img.src = this.dataset.src;
+        img.classList.add('img-responsive');
+        panel.appendChild(img);
+      });
+    }
   }
+
 }
 
 
